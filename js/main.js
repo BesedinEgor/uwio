e121b8c4016c8f88bb132f5ab51da77452c5c56d
Egor Besedin <Besedinegor@mail.ru>
2023-01-10 19:39:45 +0300

    исправления некоторых слайдеров, позиционирование некоторых блоков на десктопной версии

--- js/main.js
+++ js/main.js
@@ -1,13 +1,24 @@
-const swiper = new Swiper(".swiper", {
+const comunicationSwiper = new Swiper(".comunication-swiper", {
   slidesPerView: 1,
   loop: true,
   pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
+    el: ".comunication-swiper-pagination",
   },
+  breakpoints: {
+    1080: {
+      slidesPerView: 3,
+    },
+    
+  },
+});
+
+
+const purposesSwiper = new Swiper(".purposes-swiper", {
+  slidesPerView: 1,
+  loop: true,
   navigation: {
-    nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
+    nextEl: ".swiper-button-next",
   },
   breakpoints: {
     1080: {
@@ -16,13 +27,10 @@ const swiper = new Swiper(".swiper", {
   },
 });
 
-const reviewsSwiper = new Swiper(".reviews__swiper", {
+
+const reviewsSwiper = new Swiper(".reviews-swiper", {
   slidesPerView: 1,
   loop: true,
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
@@ -34,31 +42,9 @@ const reviewsSwiper = new Swiper(".reviews__swiper", {
   },
 });
 
-const teamSwiper = new Swiper(".team-slider__swiper", {
-  slidesPerView: 1,
-  loop: true,
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-  breakpoints: {
-    1080: {
-      slidesPerView: 1,
-    },
-  },
-});
-
-  const advantagesSwiper = new Swiper(".advantages-slider__swiper", {
+  const advantagesSwiper = new Swiper(".advantages-swiper", {
     slidesPerView: 1,
     loop: true,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
     navigation: {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev",
@@ -70,15 +56,16 @@ const teamSwiper = new Swiper(".team-slider__swiper", {
     },
   });
 
-function detectDevice() {
-  if (
-    /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
-      navigator.userAgent
-    )
-  ) {
-    // true for mobile device
-    advantagesSwiper
-  } else {
-    // false for not mobile device
-  }
-}
+const teamSwiper = new Swiper(".team-slider-swiper", {
+  slidesPerView: 1,
+  loop: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    1080: {
+      slidesPerView: 1,
+    },
+  },
+});
